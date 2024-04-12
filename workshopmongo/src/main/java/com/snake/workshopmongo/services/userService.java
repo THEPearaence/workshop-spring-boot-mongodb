632ee799621a7bb71419c0aa72672cd580cf01af
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-04-12 13:14:11 -0300

    Inserção de usuário com POST

--- workshopmongo/src/main/java/com/snake/workshopmongo/services/userService.java
+++ workshopmongo/src/main/java/com/snake/workshopmongo/services/userService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.snake.workshopmongo.domain.User;
+import com.snake.workshopmongo.dto.UserDTO;
 import com.snake.workshopmongo.repository.UserRepository;
 import com.snake.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class userService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
