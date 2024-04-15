25aa4748b4566ba462d8e4bba319c8492d4cb97d
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-04-15 11:18:18 -0300

    Acrescentando comentários aos posts

--- workshopmongo/src/main/java/com/snake/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/snake/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.snake.workshopmongo.domain.Post;
 import com.snake.workshopmongo.domain.User;
 import com.snake.workshopmongo.dto.AuthorDTO;
+import com.snake.workshopmongo.dto.CommentDTO;
 import com.snake.workshopmongo.repository.PostRepository;
 import com.snake.workshopmongo.repository.UserRepository;
 
@@ -43,6 +44,13 @@ public class Instantiation implements CommandLineRunner{
 						, "partiu Viagem", "vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post
 				(null, sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem Linda!",sdf.parse("21/03/2018"),new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("22/03/2018"),new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um bom dia!",sdf.parse("23/03/2018"),new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
--- workshopmongo/src/main/java/com/snake/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/snake/workshopmongo/resources/PostResource.java
@@ -11,7 +11,7 @@ import com.snake.workshopmongo.domain.Post;
 import com.snake.workshopmongo.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value="/posts/")
 public class PostResource {
 	
 	@Autowired
