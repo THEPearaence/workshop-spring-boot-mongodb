db0c158df14a4149862cfbfa09d293360864f29e
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-04-15 11:05:26 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/snake/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.snake.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.snake.workshopmongo.domain.Post;
+import com.snake.workshopmongo.repository.PostRepository;
+import com.snake.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
